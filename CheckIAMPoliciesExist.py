865652457f7fe4c5424afaa6415094c9bdb94c39
Keith Walker <walkerk1980@users.noreply.github.com>
2018-05-04 18:43:00 -0700

    Update CheckIAMPoliciesExist.py

--- CheckIAMPoliciesExist.py
+++ CheckIAMPoliciesExist.py
@@ -1,6 +1,3 @@
-#
-# This file made available under CC0 1.0 Universal (https://creativecommons.org/publicdomain/zero/1.0/legalcode)
-#
 # Ensure one or several specific IAM policies exist
 # Description: Checks that defined IAM policies have been defined in AWS IAM.
 #
